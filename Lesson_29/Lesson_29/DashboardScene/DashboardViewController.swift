6b87ff74f0965901ad8a9f87c5e570606349fc07
Maksim Bulat <maksim.bulat@seb.ee>
2022-07-11 19:58:08 +0200

    Implement routing for scenes

--- Lesson_29/Lesson_29/DashboardScene/DashboardViewController.swift
+++ Lesson_29/Lesson_29/DashboardScene/DashboardViewController.swift
@@ -11,19 +11,11 @@ class DashboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logoutButtonAction(_ sender: Any) {
+        if presentingViewController != nil {
+            self.dismiss(animated: true)
+        }
     }
-    */
-
 }
--- Lesson_29/Lesson_29/LoginScene/LoginViewController.swift
+++ Lesson_29/Lesson_29/LoginScene/LoginViewController.swift
@@ -8,4 +8,15 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func loginButtonAction(_ sender: Any) {
+        showNextViewController()
+    }
+    
+    private func showNextViewController() {
+        let identifier = String(describing: DashboardViewController.self)
+        let storyboard = UIStoryboard(name: identifier, bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: identifier)
+        controller.modalPresentationStyle = .fullScreen
+        present(controller, animated: true)
+    }
 }
