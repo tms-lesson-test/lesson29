296694aef91a276436f453dde0561c8dfc9c4980
Maksim Bulat <maksim.bulat@seb.ee>
2022-07-11 18:34:47 +0200

    Upload initial project

---
+++ Lesson_29/Lesson_29/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_29
+//
+//  Created by Maksim Bulat on 11/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
